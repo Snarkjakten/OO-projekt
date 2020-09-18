21a2c21eda4c2a88c0241cfc9932969e630ed600
tobiasengblom <gusengblto@student.gu.se>
2020-09-18 13:43:35 +0200

    Changed setPosition comment

--- DodgerGame/src/main/java/Movement/AbstractMovable.java
+++ DodgerGame/src/main/java/Movement/AbstractMovable.java
@@ -27,7 +27,7 @@ public abstract class AbstractMovable implements IMovable {
         this.position = position.add(velocity.getX(), velocity.getY()); // add() returns a new Point2D
     }
 
-    // Setter for spaceship position
+    // Setter for self position
     // @Author Tobias Engblom
     public void setPosition(double xPos, double yPos) {
         position = new Point2D(xPos, yPos);
