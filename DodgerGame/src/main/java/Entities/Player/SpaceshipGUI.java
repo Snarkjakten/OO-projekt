3dfde3e1bcdd4892796cfd6f90a1f9ee898c519a
tobiasengblom <gusengblto@student.gu.se>
2020-09-24 15:34:26 +0200

    Added wrap around function and moved some methods to Model

--- DodgerGame/src/main/java/Entities/Player/SpaceshipGUI.java
+++ DodgerGame/src/main/java/Entities/Player/SpaceshipGUI.java
@@ -11,9 +11,7 @@ public class SpaceshipGUI {
     private Point2D point;
     private Image image;
 
-    /**
-     * @Author Tobias Engblom
-     */
+    //@Author Tobias Engblom
     public SpaceshipGUI(Spaceship spaceship, double x, double y) {
         this.spaceship = spaceship;
         this.image = addImageToSpaceship();
@@ -21,35 +19,41 @@ public class SpaceshipGUI {
         point = new Point2D(x, y);
     }
 
+    //@Author Tobias Engblom
     public Image addImageToSpaceship() {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("spaceship.gif");
         image = new Image(inputStream);
         return image;
     }
 
-    protected Spaceship getSpaceship() {
+    //@Author Tobias Engblom
+    public Spaceship getSpaceship() {
         return spaceship;
     }
 
+    //@Author Tobias Engblom
     public void setPosition(double xPos, double yPos) {
         this.point = point.add(xPos, yPos);
         spaceship.setPosition(xPos, yPos);
     }
 
+    //@Author Tobias Engblom
     public double getXPosition() {
         return spaceship.position.getX();
     }
 
+    //@Author Tobias Engblom
     public double getYPosition() {
         return spaceship.position.getY();
     }
 
+    //@Author Tobias Engblom
     public Point2D getPoint() {
         return point;
     }
 
+    //@Author Tobias Engblom
     public Image getImage() {
         return image;
     }
-    //------------------------------------------------------------------------------------------------------------------
 }
--- DodgerGame/src/main/java/Game.java
+++ DodgerGame/src/main/java/Game.java
@@ -1,3 +1,110 @@
-public class Game {
+import Entities.Player.Spaceship;
+import Entities.Player.SpaceshipFactory;
+import Entities.Player.SpaceshipGUI;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private static Game instance = null;
+    private Spaceship spaceship = SpaceshipFactory.createSpaceship(true);
+    private SpaceshipGUI spaceshipGUI = new SpaceshipGUI(spaceship, 368, 268);
+    private Spaceship wrapAroundSpaceship = SpaceshipFactory.createSpaceship(false);
+    private SpaceshipGUI wrapAroundSpaceshipGUI = new SpaceshipGUI(wrapAroundSpaceship, 900, 900);
+    private List<Spaceship> spaceships = new ArrayList<>();
+
+    private Game() {
+    }
+
+    //@Author Tobias Engblom
+    protected static Game getInstance() {
+        if (instance == null) {
+            instance = new Game();
+            instance.initSpaceships();
+        }
+        return instance;
+    }
+
+    //@Author Tobias Engblom
+    private void initSpaceships() {
+        spaceships.add(spaceship);
+        spaceships.add(wrapAroundSpaceship);
+    }
+
+    //@Author Tobias Engblom
+    protected SpaceshipGUI getSpaceshipGUI() {
+        return spaceshipGUI;
+    }
+
+    //@Author Tobias Engblom
+    protected SpaceshipGUI getWrapAroundSpaceshipGUI() {
+        return wrapAroundSpaceshipGUI;
+    }
+
+    //@Author Tobias Engblom
+    protected List<Spaceship> getSpaceships() {
+        return spaceships;
+    }
+
+    //@Author Tobias Engblom
+    protected void wrapAround() {
+        if (spaceship.isActive() && wrapAroundSpaceship.isActive()) {
+            //TODO What happens when both ships are active and crosses the "wall"?
+        } else if (spaceship.isActive()) {
+            activateWrapAround(spaceshipGUI, wrapAroundSpaceshipGUI);
+        } else if (wrapAroundSpaceship.isActive()) {
+            activateWrapAround(wrapAroundSpaceshipGUI, spaceshipGUI);
+        }
+        if (checkInactive(spaceshipGUI))
+            spaceship.setActive(false);
+        else if (checkInactive(wrapAroundSpaceshipGUI))
+            wrapAroundSpaceship.setActive(false);
+    }
+
+    //@Author Tobias Engblom
+    //Inactivates spaceship if it's outside the map
+    private boolean checkInactive(SpaceshipGUI spaceshipGUI) {
+        return spaceshipGUI.getXPosition() < -76 || spaceshipGUI.getXPosition() > 788 || spaceshipGUI.getYPosition() < -64 || spaceshipGUI.getYPosition() > 600;
+    }
+
+    //@Author Tobias Engblom
+    private void activateWrapAround(SpaceshipGUI spaceshipGUI, SpaceshipGUI wrapAroundSpaceshipGUI) {
+        if (checkWestPosition(spaceshipGUI)) {
+            wrapAroundSpaceshipGUI.getSpaceship().setActive(true);
+            wrapAroundSpaceshipGUI.setPosition(788, spaceshipGUI.getYPosition());
+        } else if (checkEastPosition(spaceshipGUI)) {
+            wrapAroundSpaceshipGUI.getSpaceship().setActive(true);
+            wrapAroundSpaceshipGUI.setPosition(-76, spaceshipGUI.getYPosition());
+        } else if (checkNorthPosition(spaceshipGUI)) {
+            wrapAroundSpaceshipGUI.getSpaceship().setActive(true);
+            wrapAroundSpaceshipGUI.setPosition(spaceshipGUI.getXPosition(), 600);
+        } else if (checkSouthPosition(spaceshipGUI)) {
+            wrapAroundSpaceshipGUI.getSpaceship().setActive(true);
+            wrapAroundSpaceshipGUI.setPosition(spaceshipGUI.getXPosition(), -64);
+        }
+    }
+
+    //@Author Tobias Engblom
+    //Checks if active spaceship is beginning to go outside west wall
+    private boolean checkWestPosition(SpaceshipGUI spaceshipGUI) {
+        return spaceshipGUI.getXPosition() <= -12;
+    }
+
+    //@Author Tobias Engblom
+    //Checks if active spaceship is beginning to go outside north wall
+    private boolean checkNorthPosition(SpaceshipGUI spaceshipGUI) {
+        return spaceshipGUI.getYPosition() <= 0;
+    }
+
+    //@Author Tobias Engblom
+    //Checks if active spaceship is beginning to go outside east wall
+    private boolean checkEastPosition(SpaceshipGUI spaceshipGUI) {
+        return spaceshipGUI.getXPosition() >= 724;
+    }
+
+    //@Author Tobias Engblom
+    //Checks if active spaceship is beginning to go outside south wall
+    private boolean checkSouthPosition(SpaceshipGUI spaceshipGUI) {
+        return spaceshipGUI.getYPosition() >= 536;
+    }
 }
