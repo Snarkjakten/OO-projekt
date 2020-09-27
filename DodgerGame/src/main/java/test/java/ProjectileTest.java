d009dc0eb75ebd6d4230c550e10936f4f57334ce
Snarkjakten <42779066+Snarkjakten@users.noreply.github.com>
2020-09-27 02:25:28 +0200

    updated tests

---
+++ DodgerGame/src/main/java/test/java/ProjectileTest.java
@@ -0,0 +1,151 @@
+import Entities.Projectiles.*;
+import javafx.geometry.Point2D;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ProjectileTest {
+
+    SmallAsteroid projSmallAsteroid;
+    MediumAsteroid projMediumAsteroid;
+    ProjectileGUI smallAsteroidGUI;
+    Point2D startPos;
+    double deltaTime = 0.016;
+
+    @Before
+    // @Author Irja Vuorela
+    public void init() {
+        projSmallAsteroid = new SmallAsteroid();
+        projMediumAsteroid = new MediumAsteroid();
+        smallAsteroidGUI = new ProjectileGUI(ProjectileFactory.createSmallAsteroid());
+    }
+
+    @Test
+    public void testProjectileAndGuiPosition() {
+        Point2D projPosition = smallAsteroidGUI.getProjectile().position;
+        Point2D projGuiPosition = smallAsteroidGUI.getPoint();
+        assertTrue(projPosition.getX() == projGuiPosition.getX() &&
+                   projPosition.getY() == projGuiPosition.getY());
+    }
+
+    /* hardcoded values
+    @Test
+    public void testAsteroidSpeed() {
+        assertTrue(projSmallAsteroid.getSpeed() == 5);
+        assertTrue(projMediumAsteroid.getSpeed() == 3);
+    }
+    */
+
+    @Test
+    public void testAsteroidDamage() {
+        assertTrue(projSmallAsteroid.getDamage() == 20);
+        assertTrue(projMediumAsteroid.getDamage() == 35);
+    }
+
+    @Test
+    public void testAsteroidIsNotOnScreen() {
+        projSmallAsteroid.setPosition(-80, -80);
+        assertTrue(projSmallAsteroid.isNotOnScreen());
+    }
+
+    @Test
+    // Does move() move the projectile's position to the left?
+    // @Author Irja Vuorela
+    public void ProjectileMovedLeft() {
+        startPos = projSmallAsteroid.position;
+        // Negative horizontal value to move left
+        projSmallAsteroid.setHorizontal(-1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue(projSmallAsteroid.position.getX() < startPos.getX());
+    }
+
+    @Test
+    // Does move() move the projectile's position to the right?
+    // @Author Irja Vuorela
+    public void ProjectileMovedRight() {
+        startPos = projSmallAsteroid.position;
+        // Positive horizontal value to move right
+        projSmallAsteroid.setHorizontal(1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue(projSmallAsteroid.position.getX() > startPos.getX());
+    }
+
+    @Test
+    // Does move() move the projectile's position up?
+    // @Author Irja Vuorela
+    public void ProjectileMovedUp() {
+        startPos = projSmallAsteroid.position;
+        // Negative vertical value to move up
+        projSmallAsteroid.setVertical(-1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue(projSmallAsteroid.position.getY() < startPos.getY());
+    }
+
+    @Test
+    // Does move() move the projectile's position down?
+    // @Author Irja Vuorela
+    public void ProjectileMovedDown() {
+        startPos = projSmallAsteroid.position;
+        // Positive vertical value to move down
+        projSmallAsteroid.setVertical(1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue(projSmallAsteroid.position.getY() > startPos.getY());
+    }
+
+    @Test
+    // Does move() move the projectile's position up and right diagonally?
+    // @Author Irja Vuorela
+    public void ProjectileMovedUpRight() {
+        startPos = projSmallAsteroid.position;
+        projSmallAsteroid.setVertical(-1);
+        projSmallAsteroid.setHorizontal(1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue((projSmallAsteroid.position.getX() > startPos.getX()) && (projSmallAsteroid.position.getY() < startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the projectile's position up and left diagonally?
+    // @Author Irja Vuorela
+    public void ProjectileMovedUpLeft() {
+        startPos = projSmallAsteroid.position;
+        projSmallAsteroid.setVertical(-1);
+        projSmallAsteroid.setHorizontal(-1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue((projSmallAsteroid.position.getX() < startPos.getX()) && (projSmallAsteroid.position.getY() < startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the projectile's position down and right diagonally?
+    // @Author Irja Vuorela
+    public void ProjectileMovedDownRight() {
+        startPos = projSmallAsteroid.position;
+        projSmallAsteroid.setVertical(1);
+        projSmallAsteroid.setHorizontal(1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue((projSmallAsteroid.position.getX() > startPos.getX()) && (projSmallAsteroid.position.getY() > startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the projectile's position down and left diagonally?
+    // @Author Irja Vuorela
+    public void ProjectileMovedDownLeft() {
+        startPos = projSmallAsteroid.position;
+        projSmallAsteroid.setVertical(1);
+        projSmallAsteroid.setHorizontal(-1);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue((projSmallAsteroid.position.getX() < startPos.getX()) && (projSmallAsteroid.position.getY() > startPos.getY()));
+    }
+
+    @Test
+    // Does move() leave the projectile's position unchanged when its velocity was zero?
+    // @Author Irja Vuorela
+    public void ProjectileNotMovingWhenVelocityZero() {
+        startPos = projSmallAsteroid.position;
+        projSmallAsteroid.setVertical(0);
+        projSmallAsteroid.setHorizontal(0);
+        projSmallAsteroid.move(deltaTime);
+        assertTrue((projSmallAsteroid.position.getX() == startPos.getX()) && (projSmallAsteroid.position.getY() == startPos.getY()));
+    }
+}
---
+++ DodgerGame/src/main/java/test/java/SpaceshipTest.java
@@ -0,0 +1,174 @@
+import Entities.Player.Spaceship;
+import Entities.Player.SpaceshipFactory;
+import javafx.geometry.Point2D;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpaceshipTest {
+
+    Game game;
+    Spaceship spaceship;
+    Spaceship wrapAroundSpaceship;
+    Point2D startPos;
+    double deltaTime = 0.016;
+
+    @Before
+    // @Author Irja Vuorela
+    public void init() {
+        game = Game.getInstance();
+        spaceship = SpaceshipFactory.createSpaceship(true, 0, 0);
+        wrapAroundSpaceship = SpaceshipFactory.createSpaceship(false, 10000, 10000);
+    }
+
+    @Test
+    // Does move() move the ship's position to the left?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedLeft() {
+        startPos = spaceship.position;
+        spaceship.setLeft(1);
+        // Negative x value to move to the left
+        spaceship.velocity = new Point2D(-1, 0);
+        spaceship.move(deltaTime);
+        assertTrue(spaceship.position.getX() < startPos.getX());
+    }
+
+    @Test
+    // Does move() move the ship's position to the right?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedRight() {
+        startPos = spaceship.position;
+        spaceship.setRight(1);
+        // Positive x value to move to the right
+        spaceship.velocity = new Point2D(1, 0);
+        spaceship.move(deltaTime);
+        assertTrue(spaceship.position.getX() > startPos.getX());
+    }
+
+    @Test
+    // Does move() move the ship's position up?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedUp() {
+        startPos = spaceship.position;
+        spaceship.setUp(1);
+        // Negative y value to move up
+        spaceship.velocity = new Point2D(0, -1);
+        spaceship.move(deltaTime);
+        assertTrue(spaceship.position.getY() < startPos.getY());
+    }
+
+    @Test
+    // Does move() move the ship's position down?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedDown() {
+        startPos = spaceship.position;
+        spaceship.setDown(1);
+        // Positive y value to move down
+        spaceship.velocity = new Point2D(0, 1);
+        spaceship.move(deltaTime);
+        assertTrue(spaceship.position.getY() > startPos.getY());
+    }
+
+    @Test
+    // Does move() move the ship's position up and right diagonally?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedUpAndRight() {
+        startPos = spaceship.position;
+        spaceship.setUp(1);
+        spaceship.setRight(1);
+        spaceship.velocity = new Point2D(1, -1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() > startPos.getX()) && (spaceship.position.getY() < startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the ship's position up and left diagonally?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedUpAndLeft() {
+        startPos = spaceship.position;
+        spaceship.setUp(1);
+        spaceship.setLeft(1);
+        spaceship.velocity = new Point2D(-1, -1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() < startPos.getX()) && (spaceship.position.getY() < startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the ship's position down and right diagonally?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedDownAndRight() {
+        startPos = spaceship.position;
+        spaceship.setDown(1);
+        spaceship.setRight(1);
+        spaceship.velocity = new Point2D(1, 1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() > startPos.getX()) && (spaceship.position.getY() > startPos.getY()));
+    }
+
+    @Test
+    // Does move() move the ship's position down and left diagonally?
+    // @Author Irja Vuorela
+    public void SpaceshipMovedDownAndLeft() {
+        startPos = spaceship.position;
+        spaceship.setDown(1);
+        spaceship.setLeft(1);
+        spaceship.velocity = new Point2D(-1, 1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() < startPos.getX()) && (spaceship.position.getY() > startPos.getY()));
+    }
+
+    @Test
+    // Does move() leave the ship's position unchanged when attempting to move left and right simultaneously?
+    // @Author Irja Vuorela
+    public void SpaceshipNotMovingWhenLeftAndRight() {
+        startPos = spaceship.position;
+        spaceship.setLeft(1);
+        spaceship.setRight(1);
+        spaceship.velocity = new Point2D(1, 1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() == startPos.getX()) && (spaceship.position.getY() == startPos.getY()));
+    }
+
+    @Test
+    // Does move() leave the ship's position unchanged when attempting to move up and down simultaneously?
+    // @Author Irja Vuorela
+    public void SpaceshipNotMovingWhenUpAndDown() {
+        startPos = spaceship.position;
+        spaceship.setUp(1);
+        spaceship.setDown(1);
+        spaceship.velocity = new Point2D(1, 1);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() == startPos.getX()) && (spaceship.position.getY() == startPos.getY()));
+    }
+
+    @Test
+    // Does move() leave the ship's position unchanged when its velocity is zero?
+    // @Author Irja Vuorela
+    public void SpaceshipNotMovingWhenVelocityZero() {
+        startPos = spaceship.position;
+        spaceship.velocity = new Point2D(0, 0);
+        spaceship.move(deltaTime);
+        assertTrue((spaceship.position.getX() == startPos.getX()) && (spaceship.position.getY() == startPos.getY()));
+    }
+
+    @Test
+    public void westWrapAround() {
+
+    }
+
+    @Test
+    public void northWrapAround() {
+
+    }
+
+    @Test
+    public void eastWrapAround() {
+
+    }
+
+    @Test
+    public void southWrapAround() {
+
+    }
+}
