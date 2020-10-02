d530e898bc3ffdb6b8af9edd232908eea28772a2
olleWesterlund <60099045+olleWesterlund@users.noreply.github.com>
2020-10-02 15:24:08 +0200

    Added new tests for LaserBeam

--- DodgerGame/src/test/java/LaserBeamTest.java
+++ DodgerGame/src/test/java/LaserBeamTest.java
@@ -1,5 +1,9 @@
 import Entities.LaserBeam;
+import javafx.scene.image.Image;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class LaserBeamTest {
 
@@ -9,6 +13,19 @@ public class LaserBeamTest {
     //@Author Olle Westerlund
     public void init() {
         laser = new LaserBeam( 0.1);
+    }
+
+    @Test
+    public void testInitImages() {
+        for (Image image : laser.getImages()) {
+            assertTrue(image != null);
+        }
+    }
+
+    @Test
+    public void testGetFrame() {
+        Image image = laser.getFrame(0.6);
+        assertTrue(image != null);
     }
 
 }
