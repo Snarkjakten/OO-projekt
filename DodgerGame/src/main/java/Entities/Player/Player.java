193c1e9c9afc354d3c5b085472b320461d95998f
olleWesterlund <60099045+olleWesterlund@users.noreply.github.com>
2020-10-07 11:25:27 +0200

    Changed List<Spaceship> in Player to private

--- DodgerGame/src/main/java/Entities/Player/Player.java
+++ DodgerGame/src/main/java/Entities/Player/Player.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    public List<Spaceship> spaceships = new ArrayList<>();
+    private List<Spaceship> spaceships = new ArrayList<>();
     private final SimpleIntegerProperty hp = new SimpleIntegerProperty(200);
     private int points;
     private int nrOfShields;
