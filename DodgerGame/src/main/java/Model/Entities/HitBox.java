dae67636335ade4a1bfc6cff21bbd92b6ff7e240
tobiasengblom <gusengblto@student.gu.se>
2020-10-22 15:56:08 +0200

    Added javadoc for hitBox and removed width and height for hitbox

--- DodgerGame/src/main/java/Model/Entities/HitBox.java
+++ DodgerGame/src/main/java/Model/Entities/HitBox.java
@@ -5,8 +5,6 @@ import Model.Rectangle2D;
 
 public class HitBox {
 
-    private double width;
-    private double height;
     private Rectangle2D hitBox;
 
     /**
@@ -15,15 +13,10 @@ public class HitBox {
      * @author Viktor & Irja
      */
     public HitBox() {
-        //this.position = new Point2D(0, 0);
-        this.width = 1;
-        this.height = 1;
-        updateHitBox(0, 0, this.width, this.height);
+        updateHitBox(0, 0, 0, 0);
     }
 
     public HitBox(double xPos, double yPos, double width, double height) {
-        this.width = width;
-        this.height = height;
         updateHitBox(xPos, yPos, width, height);
     }
 
@@ -40,10 +33,20 @@ public class HitBox {
         setHitBox(new Rectangle2D(xPos, yPos, width, height));
     }
 
+    /**
+     * @param xPos the new xPosition for this hitBox
+     * @param yPos the new yPosition for this hitbox
+     * @author Tobias Engblom
+     */
     public void updateHitBoxPosition(double xPos, double yPos) {
-        setHitBox(new Rectangle2D(xPos, yPos, this.width, this.height));
+        setHitBox(new Rectangle2D(xPos, yPos, getHitBox().getWidth(), getHitBox().getHeight()));
     }
 
+    /**
+     * @param width  the new width for this hitBox
+     * @param height the new height for this hitBox
+     * @author Tobias Engblom
+     */
     public void updateHitBoxSize(double width, double height) {
         setHitBox(new Rectangle2D(getHitBox().getX(), getHitBox().getY(), width, height));
     }
@@ -56,13 +59,19 @@ public class HitBox {
      * @author Tobias Engblom
      */
     public void updatePosition(double xVelocity, double yVelocity) {
-        updateHitBox(this.getX() + xVelocity, this.getY() + yVelocity, this.width, this.height);
+        updateHitBox(this.getX() + xVelocity, this.getY() + yVelocity, getHitBox().getWidth(), getHitBox().getHeight());
     }
 
+    /**
+     * @return the xPosition of this hitBox
+     */
     public double getX() {
         return this.hitBox.getX();
     }
 
+    /**
+     * @return the yPosition of this hitBox
+     */
     public double getY() {
         return this.hitBox.getY();
     }
@@ -75,23 +84,24 @@ public class HitBox {
         this.hitBox = hitBox;
     }
 
+    /**
+     * @return the 2D rectangle of this hitBox
+     */
     public Rectangle2D getHitBox() {
         return hitBox;
     }
 
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
+    /**
+     * @return the hitBox width
+     */
     public double getWidth() {
-        return width;
+        return getHitBox().getWidth();
     }
 
+    /**
+     * @return the hitBox height
+     */
     public double getHeight() {
-        return height;
+        return getHitBox().getHeight();
     }
 }
